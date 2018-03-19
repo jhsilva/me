ad7c355c0f752b92ddbb3b189d84b8f03ecd9210
jhsilva <talkto@jhonatansilva.com>
2018-03-18 23:24:41 -0300

    feat: set routes and pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,47 @@ import Loadable from 'react-loadable';
 import { Route } from 'react-router-dom';
 
 const MenuLoadableComponent = Loadable({
-  loader: () => import('./components/Menu' /* webpackChunkName: 'menu' */),
+  loader: () => import('./components/Menu' /* webpackChunkName: 'Menu' */),
   loading() {
     return <div>Loading...</div>;
   },
 });
 
 const PageHomeLoadableComponent = Loadable({
-  loader: () => import('./components/PageHome' /* webpackChunkName: 'home' */),
+  loader: () =>
+    import('./components/PageHome' /* webpackChunkName: 'PageHome' */),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+
+const PageAboutLoadableComponent = Loadable({
+  loader: () =>
+    import('./components/PageAbout' /* webpackChunkName: 'PageAbout' */),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+
+const PageExperienceLoadableComponent = Loadable({
+  loader: () =>
+    import('./components/PageExperience' /* webpackChunkName: 'PageExperience' */),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+
+const PageContactLoadableComponent = Loadable({
+  loader: () =>
+    import('./components/PageContact' /* webpackChunkName: 'PageContact' */),
+  loading() {
+    return <div>Loading...</div>;
+  },
+});
+
+const PageLabLoadableComponent = Loadable({
+  loader: () =>
+    import('./components/PageLab' /* webpackChunkName: 'PageLab' */),
   loading() {
     return <div>Loading...</div>;
   },
@@ -20,6 +53,10 @@ const Routes = () => (
   <div>
     <MenuLoadableComponent />
     <Route exact path="/" component={PageHomeLoadableComponent} />
+    <Route path="/about" component={PageAboutLoadableComponent} />
+    <Route path="/experience" component={PageExperienceLoadableComponent} />
+    <Route path="/contact" component={PageContactLoadableComponent} />
+    <Route path="/lab" component={PageLabLoadableComponent} />
   </div>
 );
 
